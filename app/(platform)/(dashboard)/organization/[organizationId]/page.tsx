f106735131380b035590b5b39fab4617f64597c8
Shota240 <shoup.8519@gmail.com>
2024-05-03 19:36:36 +0900

    feat: フォームの中身をコンポーネント化、useFormStatusに応じたdisable制御

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,17 +1,18 @@
-import { create } from "@/actions/create-board";
+import { db } from "@/lib/db";
+import { Board } from "./board";
+import { Form } from "./form";
 
-const organizationIdPage = () => {
+const organizationIdPage = async () => {
+  const boards = await db.board.findMany();
+
   return (
-    <div>
-      <form action={create}>
-        <input
-          id="title"
-          name="title"
-          required
-          placeholder="タイトルを入力してください.."
-          className="border-black border p-1"
-        />
-      </form>
+    <div className="flex flex-col space-y-4">
+      <Form />
+      <div className="space-y-2">
+        {boards.map((board) => (
+          <Board key={board.id} title={board.title} id={board.id} />
+        ))}
+      </div>
     </div>
   );
 };
