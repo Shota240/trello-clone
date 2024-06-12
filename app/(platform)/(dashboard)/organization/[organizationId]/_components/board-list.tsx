a48a1491affba239adcee1c1d47fb7594d486610
Shota240 <shoup.8519@gmail.com>
2024-06-13 02:28:09 +0900

    feat: ボード作成フォーム追加、 ボード背景画像をUnsplash経由で取得して表示

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -1,3 +1,4 @@
+import { FormPopover } from "@/components/form/form-popover";
 import { Hint } from "@/components/hint";
 import { User2, HelpCircle } from "lucide-react";
 
@@ -9,21 +10,23 @@ export const BoardList = () => {
         ボード一覧
       </div>
       <div className="grid grid-cols2 sm:grid-cols3 lg:grid-cols-4 gap-4">
-        <div
-          role="button"
-          className="aspect-video relative h-full w-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition"
-        >
-          <p className="text-sm">ボードを作成</p>
-          <span className="text-xs">残り5個</span>
-          <Hint
-            sideOffset={40}
-            description={`
+        <FormPopover sideOffset={10} side="right">
+          <div
+            role="button"
+            className="aspect-video relative h-full w-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition"
+          >
+            <p className="text-sm">ボードを作成</p>
+            <span className="text-xs">残り5個</span>
+            <Hint
+              sideOffset={40}
+              description={`
           フリーのワークスペースの場合、オープンボードを5個まで作成できます。上限を開放するには、アップグレードしてください。
           `}
-          >
-            <HelpCircle className="absolute bottom-2 right-2 h-[14px] w-[14px]" />
-          </Hint>
-        </div>
+            >
+              <HelpCircle className="absolute bottom-2 right-2 h-[14px] w-[14px]" />
+            </Hint>
+          </div>
+        </FormPopover>
       </div>
     </div>
   );
