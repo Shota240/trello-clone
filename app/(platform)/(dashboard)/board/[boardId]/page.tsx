0798bd7191efd695d8b180d476b2974ddb73b31d
Shota240 <shoup.8519@gmail.com>
2024-06-19 20:44:44 +0900

    feat: リスト、カードのpageとcontainerコンポーネント作成

--- app/(platform)/(dashboard)/board/[boardId]/page.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -1,7 +1,52 @@
-const BoardPage = () => {
-  return (
-    <div>BoardID</div>
-  )
-}
+import { auth } from "@clerk/nextjs/server";
 
-export default BoardPage;;
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+import { ListContainer } from "./_components/list-container";
+
+interface BoardIdPageProps {
+  params: {
+    boardId: string;
+  };
+};
+
+const BoardPage = async ({
+  params
+}: BoardIdPageProps) => {
+  const {orgId} = auth();
+
+  if (!orgId) {
+    redirect("/select-org");
+  }
+
+  const lists = await db.list.findMany({
+    where: {
+      boardId: params.boardId,
+      board: {
+        orgId,
+      },
+    },
+    include: {
+      cards: {
+        orderBy: {
+          order: "asc",
+        }
+      }
+    },
+    orderBy: {
+      order: "asc"
+    }
+  })
+
+
+  return (
+    <div className="p-4 h-full overflow-x-auto">
+      <ListContainer
+      boardId={params.boardId}
+      data={lists}
+      />
+    </div>
+  );
+};
+
+export default BoardPage;
