62dd5dca567c44ec3df1a9c2eb58b6a9babd3e47
Shota240 <shoup.8519@gmail.com>
2024-06-26 01:34:34 +0900

    feat: リストオプション枠組み作成、リスト削除機能実装

---
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-options.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import { List } from "@prisma/client";
+import { MoreHorizontal, X } from "lucide-react";
+
+import {
+  Popover,
+  PopoverClose,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { useAction } from "@/hooks/use-action";
+import { deleteList } from "@/actions/delete-list";
+import { Button } from "@/components/ui/button";
+import { FormSubmit } from "@/components/form/form-submit";
+import { Separator } from "@/components/ui/separator";
+import { toast } from "sonner";
+
+interface ListOptionProps {
+  data: List;
+  onAddCard: () => void;
+}
+
+export const ListOptions = ({ data, onAddCard }: ListOptionProps) => {
+  const { execute: executeDelete } = useAction(deleteList, {
+    onSuccess: (data) => {
+      toast.success(`リスト"${data.title}"を削除しました`);
+    },
+    onError: (error) => {
+      toast.error(error);
+    },
+  });
+
+  const onDelete = (formData: FormData) => {
+    const id = formData.get("id") as string;
+    const boardId = formData.get("boardId") as string;
+
+    executeDelete({id, boardId});
+  };
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button className="h-auto w-auto p-2" variant="ghost">
+          <MoreHorizontal className="h-4 w-4" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="px-0 pt-3 pb-3" side="bottom">
+        <div className="text-sm font-medium text-center text-neutral-600 pb-4">
+          リストアクション
+        </div>
+        <PopoverClose asChild>
+          <Button
+            className="h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600"
+            variant="ghost"
+          >
+            <X className="h-4 w-4" />
+          </Button>
+        </PopoverClose>
+        <Button
+          onClick={onAddCard}
+          className="rounded-none w-full h-auto p-2 px-5 justify-start font-normal text-sm"
+          variant="ghost"
+        >
+          カードを追加...
+        </Button>
+        <form>
+          <input hidden name="id" id="id" value={data.id} />
+          <input hidden name="boardId" id="boardId" value={data.boardId} />
+          <FormSubmit
+            className="rounded-none w-full h-auto p-2 px-5 justify-start font-normal text-sm"
+            variant="ghost"
+          >
+            このリストをコピー...
+          </FormSubmit>
+        </form>
+        <Separator />
+        <form action={onDelete}>
+          <input hidden name="id" id="id" value={data.id} />
+          <input hidden name="boardId" id="boardId" value={data.boardId} />
+          <FormSubmit
+            className="rounded-none w-full h-auto p-2 px-5 justify-start font-normal text-sm"
+            variant="ghost"
+          >
+            このリストを削除
+          </FormSubmit>
+        </form>
+      </PopoverContent>
+    </Popover>
+  );
+};
