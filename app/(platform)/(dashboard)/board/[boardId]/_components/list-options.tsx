0e2c31dfb5e230034959f9d750b7ba50fe225200
Shota240 <shoup.8519@gmail.com>
2024-06-26 01:58:20 +0900

    feat: リストコピー機能追加

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-options.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-options.tsx
@@ -15,6 +15,8 @@ import { Button } from "@/components/ui/button";
 import { FormSubmit } from "@/components/form/form-submit";
 import { Separator } from "@/components/ui/separator";
 import { toast } from "sonner";
+import { ElementRef, useRef } from "react";
+import { copyList } from "@/actions/copy-list";
 
 interface ListOptionProps {
   data: List;
@@ -22,9 +24,22 @@ interface ListOptionProps {
 }
 
 export const ListOptions = ({ data, onAddCard }: ListOptionProps) => {
+  const closeRef = useRef<ElementRef<"button">>(null);
+
   const { execute: executeDelete } = useAction(deleteList, {
     onSuccess: (data) => {
       toast.success(`リスト"${data.title}"を削除しました`);
+      closeRef.current?.click();
+    },
+    onError: (error) => {
+      toast.error(error);
+    },
+  });
+
+  const { execute: executeCopy } = useAction(copyList, {
+    onSuccess: (data) => {
+      toast.success(`リスト"${data.title}"をコピーしました`);
+      closeRef.current?.click();
     },
     onError: (error) => {
       toast.error(error);
@@ -35,7 +50,14 @@ export const ListOptions = ({ data, onAddCard }: ListOptionProps) => {
     const id = formData.get("id") as string;
     const boardId = formData.get("boardId") as string;
 
-    executeDelete({id, boardId});
+    executeDelete({ id, boardId });
+  };
+
+  const onCopy = (formData: FormData) => {
+    const id = formData.get("id") as string;
+    const boardId = formData.get("boardId") as string;
+
+    executeCopy({ id, boardId });
   };
 
   return (
@@ -49,7 +71,7 @@ export const ListOptions = ({ data, onAddCard }: ListOptionProps) => {
         <div className="text-sm font-medium text-center text-neutral-600 pb-4">
           リストアクション
         </div>
-        <PopoverClose asChild>
+        <PopoverClose ref={closeRef} asChild>
           <Button
             className="h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600"
             variant="ghost"
@@ -64,7 +86,7 @@ export const ListOptions = ({ data, onAddCard }: ListOptionProps) => {
         >
           カードを追加...
         </Button>
-        <form>
+        <form action={onCopy}>
           <input hidden name="id" id="id" value={data.id} />
           <input hidden name="boardId" id="boardId" value={data.boardId} />
           <FormSubmit
